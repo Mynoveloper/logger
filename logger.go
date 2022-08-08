94a3c9973248b83b1f2ca2042610dfe181321fa9
Mynor Castrillo <mcastrillo@plusti.com>
2022-08-07 23:19:55 -0600

    standar logger using logrus

--- logger.go
+++ logger.go
@@ -1,183 +1,161 @@
 package logger
 
 import (
-	"log"
+	"errors"
+	"io"
 	"os"
+	"path"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
-type loggerOptions struct {
-	LogFileIsActive bool   `json:"logFileIsActive"` // If you need write to file
-	LogFileName     string `json:"logFileName"`     // Filename that you want to write the logs
-	LogLevel        string `json:"logLevel"`        // Log level that you want write
+type Level uint32
+
+var AllLevels = []Level{
+	PanicLevel,
+	FatalLevel,
+	ErrorLevel,
+	WarnLevel,
+	InfoLevel,
+	DebugLevel,
+	TraceLevel,
 }
 
-var logOptions loggerOptions
-
-var (
-	Debug *log.Logger
-
-	// Info Default level to write output
-	Info             *log.Logger
-	Warn             *log.Logger
-	Error            *log.Logger
-	internalDebugger *log.Logger
+const (
+	PanicLevel Level = iota
+	FatalLevel       = 1
+	ErrorLevel       = 2
+	WarnLevel        = 3
+	InfoLevel        = 4
+	DebugLevel       = 5
+	TraceLevel       = 6
 )
 
-// private variables to set output defined on setLevel caller from setLoggerOptions
-var (
-	debugLoggerOutput   *os.File
-	infoLoggerOutput    *os.File
-	warningLoggerOutput *os.File
-	errorLoggerOutput   *os.File
-)
+type LoggerInfo struct {
+	ProgramName string
+	FileName    string
+	Level       string
+	LogToFile   bool
+	PrettyPrint bool
+	JSONOutput  bool
+}
 
-// set level to write output
-func setLevel() {
-	logOptions.LogLevel = os.Getenv("LOG_LEVEL")
+type Logger struct {
+	logger *logrus.Entry
+}
 
-	switch strings.ToLower(logOptions.LogLevel) {
-	case "debug", "debugger":
-		if !logOptions.LogFileIsActive {
-			debugLoggerOutput = os.Stdout
-			infoLoggerOutput = os.Stdout
-			warningLoggerOutput = os.Stdout
-			errorLoggerOutput = os.Stderr
-		}
+type ILogger interface {
+	Debug(...interface{})
+	Info(...interface{})
+	Warning(...interface{})
+	Error(...interface{})
+	Panic(...interface{})
+	Fatal(...interface{})
+}
 
-		Debug = log.New(debugLoggerOutput, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Info = log.New(infoLoggerOutput, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Warn = log.New(warningLoggerOutput, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Error = log.New(errorLoggerOutput, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-		break
-	case "info", "information":
-		if !logOptions.LogFileIsActive {
-			infoLoggerOutput = os.Stdout
-			warningLoggerOutput = os.Stdout
-			errorLoggerOutput = os.Stderr
-		}
-
-		Debug = log.New(os.Stdin, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Info = log.New(infoLoggerOutput, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Warn = log.New(warningLoggerOutput, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Error = log.New(errorLoggerOutput, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-		break
+// Private function to get
+func getLevel(level string) Level {
+	level = strings.ToLower(level)
+	switch strings.ToLower(level) {
+	case "panic":
+		return PanicLevel
+	case "fatal":
+		return FatalLevel
+	case "error":
+		return ErrorLevel
 	case "warn", "warning":
-		if !logOptions.LogFileIsActive {
-			warningLoggerOutput = os.Stdout
-			errorLoggerOutput = os.Stderr
-		}
-
-		Debug = log.New(os.Stdin, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Info = log.New(os.Stdin, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Warn = log.New(warningLoggerOutput, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Error = log.New(errorLoggerOutput, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-		break
-	case "err", "error":
-		if !logOptions.LogFileIsActive {
-			errorLoggerOutput = os.Stderr
-		}
-
-		Debug = log.New(os.Stdin, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Info = log.New(os.Stdin, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Warn = log.New(os.Stdin, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Error = log.New(errorLoggerOutput, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-		break
+		return WarnLevel
+	case "info":
+		return InfoLevel
+	case "debug":
+		return DebugLevel
+	case "trace":
+		return TraceLevel
 	default:
-		if !logOptions.LogFileIsActive {
-			debugLoggerOutput = os.Stdin
-			infoLoggerOutput = os.Stdout
-			warningLoggerOutput = os.Stdout
-			errorLoggerOutput = os.Stderr
-		}
-
-		Debug = log.New(debugLoggerOutput, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Info = log.New(infoLoggerOutput, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Warn = log.New(warningLoggerOutput, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-		Error = log.New(errorLoggerOutput, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-		break
+		return DebugLevel
 	}
 }
 
-// set logger options from the file configuration
-func setLoggerOptions() {
-
-	// if the LogFileIsActive is true, write to the specific file
-	if logOptions.LogFileIsActive {
-
-		// Get path from executable file
-		ex, err := os.Executable()
-		if err != nil {
-			internalDebugger.Println(err)
-		}
-
-		// Extract relative path of executable file
-		exeFilePath := filepath.Dir(ex)
-		pathLoggerFile := exeFilePath + "\\log\\" + logOptions.LogFileName
-
-		// Open or create file in the path of loggerFile
-		output, err := os.OpenFile(pathLoggerFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-		if err != nil {
-			internalDebugger.Println(err)
-		}
-
-		debugLoggerOutput = output
-		infoLoggerOutput = output
-		warningLoggerOutput = output
-		errorLoggerOutput = output
-	}
-
-	setLevel()
-}
-
-func contains(s []string, str string) bool {
-	for _, v := range s {
-		if v == str {
-			return true
-		}
-	}
-
-	return false
-}
-
-func init() {
-	// If error has occurred write on logger_error.log in the system path
-	internalOutputDebugger, _ := os.OpenFile("logger_error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	internalDebugger = log.New(internalOutputDebugger, "LOGGER: ", log.Ldate|log.Ltime|log.Lshortfile)
-	supportedFileExtensions := []string{
-		"txt",
-		"log",
-	}
-
-	var err error
-	logOptions.LogFileIsActive, err = strconv.ParseBool(os.Getenv("LOG_FILE_IS_ACTIVE"))
+func logToFile(fileName string) (io.Writer, error) {
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		logOptions.LogFileIsActive = false
+		return file, errors.New("Can't write in " + fileName)
 	}
 
-	logOptions.LogFileName = os.Getenv("LOG_FILE_NAME")
+	return file, nil
+}
 
-	// Not exist file for logger --> logger.log as default name of file
-	if len(logOptions.LogFileName) == 0 {
-		logOptions.LogFileName = "logger.log"
-	} else {
+func NewLogger(loggerInfo LoggerInfo) ILogger {
 
-		// File only contain one extension
-		elementsInFileName := strings.Split(logOptions.LogFileName, ".")
-		if len(elementsInFileName) > 2 || len(elementsInFileName) < 2 {
-			log.Println("The file name", logOptions.LogFileName, " is not a correct format, use similar to this: logger.log")
-			log.Println("The file name has changed to logger.log")
-			logOptions.LogFileName = "logger.log"
+	var baseLogger = logrus.New()
+
+	if len(loggerInfo.ProgramName) == 0 {
+		currentPathExe, _ := os.Executable()
+		loggerInfo.ProgramName = filepath.Base(currentPathExe)
+	}
+
+	logger := baseLogger.WithFields(logrus.Fields{
+		"program": loggerInfo.ProgramName,
+	})
+
+	logger.Logger.SetLevel(logrus.Level(getLevel(loggerInfo.Level)))
+
+	if loggerInfo.LogToFile {
+		writer, err := logToFile(loggerInfo.FileName)
+		if err != nil {
+			logger.Logger.Errorln(err)
 		} else {
-			// Evaluate the extensionFile
-			if !contains(supportedFileExtensions, elementsInFileName[1]) {
-				log.Println("The file extension is not supported, so extension has changed to .log")
-				logOptions.LogFileName = elementsInFileName[1] + ".log"
-			}
+			logger.Logger.Out = writer
 		}
 	}
 
-	setLoggerOptions()
+	if loggerInfo.JSONOutput {
+		logger.Logger.Formatter = &logrus.JSONFormatter{
+			PrettyPrint: loggerInfo.PrettyPrint,
+		}
+	}
+
+	return &Logger{
+		logger: logger,
+	}
+}
+
+func (l *Logger) Debug(args ...interface{}) {
+	_, fileName, numberLine, _ := runtime.Caller(1)
+	l.logger = l.logger.WithField("location", path.Base(fileName)+":"+strconv.Itoa(numberLine))
+	l.logger.Debug(args...)
+}
+
+func (l *Logger) Info(args ...interface{}) {
+	_, fileName, numberLine, _ := runtime.Caller(1)
+	l.logger = l.logger.WithField("location", path.Base(fileName)+":"+strconv.Itoa(numberLine))
+	l.logger.Info(args...)
+}
+
+func (l *Logger) Warning(args ...interface{}) {
+	_, fileName, numberLine, _ := runtime.Caller(1)
+	l.logger = l.logger.WithField("location", path.Base(fileName)+":"+strconv.Itoa(numberLine))
+	l.logger.Warn(args...)
+}
+
+func (l *Logger) Error(args ...interface{}) {
+	_, fileName, numberLine, _ := runtime.Caller(1)
+	l.logger = l.logger.WithField("location", path.Base(fileName)+":"+strconv.Itoa(numberLine))
+	l.logger.Error(args...)
+}
+
+func (l *Logger) Panic(args ...interface{}) {
+	_, fileName, numberLine, _ := runtime.Caller(1)
+	l.logger = l.logger.WithField("location", path.Base(fileName)+":"+strconv.Itoa(numberLine))
+	l.logger.Panic(args...)
+}
+
+func (l *Logger) Fatal(args ...interface{}) {
+	_, fileName, numberLine, _ := runtime.Caller(1)
+	l.logger = l.logger.WithField("location", path.Base(fileName)+":"+strconv.Itoa(numberLine))
+	l.logger.Fatalln(args...)
 }
